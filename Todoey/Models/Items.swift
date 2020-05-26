f314c532136ef9ee87f910a62054bd4d50cad2b4
Abhas Kumar <abhasroy@gmail.com>
2020-05-25 21:17:22 -0700

    Added MVC and adding properties for itemArray in class item

---
+++ Todoey/Models/Items.swift
@@ -0,0 +1,13 @@
+//
+//  Items.swift
+//  Todoey
+//
+//  Created by Abhas Kumar on 5/25/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+class Items {
+    var title: String = ""
+    var done: Bool = false
+}
